4459c32772a4f6935eaa6d3eb994681c18347a7c
mtpuoc <92272427+mtpuoc@users.noreply.github.com>
2021-10-12 21:56:51 +0200

    readme and avaluation python

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# exchangeRateADScraper
+# Pràctica 1: Web scraping
+Aquest treball s'ha realitzat per una pràctica de l'assignatura _Tipologia i cicle de vida de les dades_ del 
+Màster en Ciència de dades de la Universitat Oberta de Catalunya. 
+El treball es centra en aplicar les tècniques de web scraping per a recuperar la informació del preu de la moneda que es publica mensualment 
+al butlleti oficial i poder centralitzar-ho tota aquesta informació en un dataset. També hi ha un python per a poder fer un analisis inicial 
+i poder aplicar unes técniques o revisar si esta dintre dels termes legals.
+
+
+## Primers passos
+Per poder utilitzar **el projecte exchangeRateADScraper** es necessita tenir instalat les següents llibreries:
+````
+pip install python-whois
+pip install builtwith
+````
+
+## Estructura del projecte
+El projecte conte les següents carpetes:
+* **docs/**: Aquesta carpeta conté dos fitxers: el PDF amb les preguntes que demana la pràctica i l'altre el fitxer CSV que ha generat el proces python.
+* **src/**: Aquesta carpeta conté els procesos de python per a poder generar el fitxer CSV amb la informació que s'ha recuperat de la web. Al punt següent s'entrara en detall dels fitxers python.
+* **test/**: Aquesta carpeta conté proves que s'han realitzat per detectar errors i analitzar el funcionament de algunes funcions.
+
+### Fitxers python
+Dintre de la carpeta de source(src) hi ha els següents pythons:
+1. **main.py** es el proces principal per executar
+2. **avaluation.py**
+3. **csvfile**
+4. **global_vars**
+5. **functions**
+6. **vars**
+## Execució programa
+````
+python main.py <action:(scraping|analize)>
+````
+## Persones del equip
+El projecte ha sigut realitzat per Marc T.
+
+
+
+
+
---
+++ src/exchangeRateADScraper/avaluation.py
@@ -0,0 +1,54 @@
+import requests
+import global_vars
+import builtwith
+import whois
+from time import sleep
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+class Avaluation:
+    def __init__(self):
+        pass
+    def __is_available_in_robots(self, web):
+        is_available = True
+        res = requests.get(web.get("web") + "robots.txt", headers=global_vars.headers, verify=False)
+        if res.status_code == 200 and web.get("contains") in res.text:
+            is_available = False
+        return is_available
+    def __is_available_sitemap(self,web):
+        is_available = True
+        res = requests.get(web.get("web") + "sitemap.xml", headers=global_vars.headers, verify=False)
+        if res.status_code == 200 and web.get("contains") in res.text:
+            is_available = False
+        return is_available
+    def __get_info_web(self,web):
+        return builtwith.builtwith(web.get("web"))
+    def __who_is(self,web):
+        who_is = None
+        try:
+            who_is = whois.query(web.get("web"))
+        except whois.exceptions.UnknownTld:
+            pass
+        return who_is
+    def do(self):
+        webs = [{"web": "http://www.duana.ad/", "contains": "canvis-monetaris"},
+                     {"web": "https://www.bopa.ad/", "contains": "/bopa/"}]
+        for web in webs:
+            print("checking the page:", web)
+
+            r = self.__is_available_in_robots(web)
+            sleep(global_vars.second_sleep)
+            print("\trobots.txt is available", r)
+
+            r = self.__is_available_sitemap(web)
+            sleep(global_vars.second_sleep)
+            print("\tsitemap.xml", r)
+
+            r = self.__get_info_web(web)
+            print("\tInformation web:", r)
+            sleep(global_vars.second_sleep)
+
+            r = self.__who_is(web)
+            print("\twho is:", r)
+            sleep(global_vars.second_sleep*2)
